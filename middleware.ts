f1a23328ae2fb2fdee67671291e4ab3a2ec2c594
thefx3 <fx.pairault@yahoo.fr>
2026-01-19 00:42:29 +0100

    Remove middleware.ts (use proxy.ts)

--- middleware.ts
+++
@@ -1,8 +0,0 @@
-// middleware.ts (racine)
-import { proxy } from "@/lib/supabase/proxy";
-
-export { proxy as middleware };
-
-export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
-};
